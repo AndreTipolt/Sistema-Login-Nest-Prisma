ea6700bcedfa58603d5d4b608d610733f00a2d2b
AndreTipolt <andretipoltlopes@gmail.com>
2023-03-21 16:31:13 -0300

    Create login and Create User added

---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    }
+    const newUser = this.prisma.user.create({
+      data: user,
+    })
+    return 'Created'
+  }
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email }
+    })
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
