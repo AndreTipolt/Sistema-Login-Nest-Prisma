198cad714b46e978201b26a1a98ad7987ddd248b
AndreTipolt <andretipoltlopes@gmail.com>
2023-03-22 09:52:08 -0300

    implemented jwt validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { JwtModule } from '@nestjs/jwt/dist';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt-strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
@@ -11,6 +12,6 @@ import { LocalStrategy } from './strategies/local.strategy';
     signOptions: { expiresIn: '8h' }
   })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
